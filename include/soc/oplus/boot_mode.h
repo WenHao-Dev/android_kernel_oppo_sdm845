518aecccbfad1290d7db8de3981684bd92e34806
WenHao2130 <wenhao2130@outlook.com>
2026-09-05 16:35:35 +0800

    soc: oplus: Import oppo soc modules
    Change-Id: Ide33d700ace873792f9a53a5de5b8fbb70ba52aa
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

---
+++ include/soc/oplus/boot_mode.h
@@ -0,0 +1,35 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2018-2020 Oplus. All rights reserved.
+ */
+/************************************************************************************
+** File: - android\kernel\arch\arm\mach-msm\include\mach\oppo_boot.h
+** 
+** Description:  
+**     change define of boot_mode here for other place to use it
+** Version: 1.0 
+************************************************************************************/
+#ifndef _OPPO_BOOT_H
+#define _OPPO_BOOT_H
+
+enum{
+        MSM_BOOT_MODE__NORMAL,
+        MSM_BOOT_MODE__FASTBOOT,
+        MSM_BOOT_MODE__RECOVERY,
+        MSM_BOOT_MODE__FACTORY,
+        MSM_BOOT_MODE__RF,
+        MSM_BOOT_MODE__WLAN,
+        MSM_BOOT_MODE__MOS,
+        MSM_BOOT_MODE__CHARGE,
+        MSM_BOOT_MODE__SILENCE,
+        MSM_BOOT_MODE__SAU,
+        MSM_BOOT_MODE__AGING = 998,
+        MSM_BOOT_MODE__SAFE = 999,
+};
+
+extern int get_boot_mode(void);
+/* add for charge */
+extern bool qpnp_is_power_off_charging(void);
+/* add for detect charger when reboot */
+extern bool qpnp_is_charger_reboot(void);
+#endif  /*_OPPO_BOOT_H*/
